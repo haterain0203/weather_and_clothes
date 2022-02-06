53074476d088b4a3fffd1e151777f310927c9ca3
haterain0203 <haterain0203@gmail.com>
2022-02-06 17:16:29 +0900

    #1 レイアウト修正

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -15,39 +15,32 @@ class HomePage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.settings),
-                    onPressed: () {
-                      //TODO 押下処理の追加
-                    },
-                  ),
+                IconButton(
+                  icon: const Icon(Icons.settings, size: 24.0,),
+                  onPressed: () {
+                    //TODO 押下処理の追加
+                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.share),
-                    onPressed: () {
-                      //TODO 押下処理の追加
-                    },
-                  ),
+                IconButton(
+                  icon: const Icon(Icons.share, size: 24.0,),
+                  onPressed: () {
+                    //TODO 押下処理の追加
+                  },
                 ),
+                const SizedBox(width: 8.0,),
               ],
             ),
 
             //AddressArea
-            //TODO 設定画面で入力された郵便番号を取得して表示
-            const Text("〒000-0000", style: TextStyle(fontSize: 22.0),),
-            //TODO 設定画面で入力された住所を取得して表示
-            const Text("東京都葛飾区", style: TextStyle(fontSize: 22.0),),
+            //TODO 設定画面で入力された郵便番号と住所を取得して表示
+            const Text("〒000-0000 東京都葛飾区", style: TextStyle(fontSize: 20.0),),
 
-            const SizedBox(height: 30.0,),
+            const SizedBox(height: 20.0,),
 
             //DateArea
             //TODO 本日の日付を取得して表示
             //TODO レイアウト的に数字のみフォントを大きくする予定
-            const Text("2月3日（木）", style: TextStyle(fontSize: 28.0),),
+            const Text("2月3日（木）", style: TextStyle(fontSize: 24.0),),
 
             //WeatherArea
             Row(
@@ -55,14 +48,14 @@ class HomePage extends StatelessWidget {
               children: [
                 //TODO 天気に合わせて画像を変更する
                 SizedBox(
-                  width: 100.0,
-                  height: 100.0,
+                  width: 70.0,
+                  height: 70.0,
                   child: Image.asset("assets/images/sun.png"),
                 ),
-                const SizedBox(width: 30.0,),
+                const SizedBox(width: 20.0,),
                 //TODO 取得した気温を表示する
                 //TODO レイアウト的に数字のみフォントを大きくする予定
-                const Text("10℃", style: TextStyle(fontSize: 28.0),)
+                const Text("10℃", style: TextStyle(fontSize: 24.0),)
               ],
             ),
 
