0bd496203f80949ab4ae64d2a18a33457861c645
haterain0203 <haterain0203@gmail.com>
2022-02-06 17:38:54 +0900

    #14 Providerの追加とHomeModelの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:weatherandclothes/home/home_model.dart';
 import 'my_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeModel()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
