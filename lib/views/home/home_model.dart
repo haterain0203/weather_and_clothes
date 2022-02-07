082135b3d4dff600e275f29cc1c431de939367eb
haterain0203 <haterain0203@gmail.com>
2022-02-07 09:45:34 +0900

    #8 住所取得ロジック作成

--- lib/views/home/home_model.dart
+++ lib/views/home/home_model.dart
@@ -1,12 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
 
 class HomeModel extends ChangeNotifier {
 
-  Future<String> getAddress(String zipCode) {
+  late String address;
+
+  Future<void> getAddress(String zipCode) async {
+    //引数の郵便番号をもとにAPI用のURLを生成
     String url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode";
 
+    //生成したURLでAPIを叩き、住所を取得
     try {
-
+      var result = await get(Uri.parse(url));
+      Map<String, dynamic> data = jsonDecode(result.body);
+      final prefecture = data["results"][0]["address1"];
+      final city = data["results"][0]["address2"];
+      address = prefecture & " " & city;
+      notifyListeners();
     } catch(e) {
       print(e);
     }
