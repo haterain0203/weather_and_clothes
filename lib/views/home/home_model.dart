445ea7a7f158b0629dda7d4e4f93267c0273ce57
haterain0203 <haterain0203@gmail.com>
2022-02-07 09:52:10 +0900

    #8 HomeRepositoryの作成とそれに伴う移管

--- lib/views/home/home_model.dart
+++ lib/views/home/home_model.dart
@@ -2,26 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
+import 'package:weatherandclothes/repositories/home_repository.dart';
 
 class HomeModel extends ChangeNotifier {
 
+  final repository = HomeRepository();
   late String address;
 
+  //郵便番号から住所を取得する処理
   Future<void> getAddress(String zipCode) async {
-    //引数の郵便番号をもとにAPI用のURLを生成
-    String url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode";
-
-    //生成したURLでAPIを叩き、住所を取得
-    try {
-      var result = await get(Uri.parse(url));
-      Map<String, dynamic> data = jsonDecode(result.body);
-      final prefecture = data["results"][0]["address1"];
-      final city = data["results"][0]["address2"];
-      address = prefecture & " " & city;
-      notifyListeners();
-    } catch(e) {
-      print(e);
-    }
+    address = await repository.getAddress(zipCode);
+    notifyListeners();
   }
-
 }
